adffdd0d495ba2a9eb7449f3b5ccdc0e37692a78
Odin <odinuv@gmail.com>
2018-02-06 01:46:13 +0100

    fix: typo

--- deploy.sh
+++ deploy.sh
@@ -6,5 +6,5 @@ docker login -u="$QUAY_USERNAME" -p="$QUAY_PASSWORD" quay.io
 docker tag component-generator quay.io/keboola/component-generator:${TRAVIS_TAG}
 docker tag component-generator quay.io/keboola/component-generator:latest
 docker images
-docker push quay.io/kebooa/component-generator:${TRAVIS_TAG}
-docker push quay.io/kebooa/component-generator:latest
+docker push quay.io/keboola/component-generator:${TRAVIS_TAG}
+docker push quay.io/keboola/component-generator:latest
