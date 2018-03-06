5e2eda8317d1b41e5c35c3d4f9e6da8edbab67c3
Tomáš Fejfar <tomas.fejfar@gmail.com>
2018-03-06 16:59:03 +0000

    php-component template

---
+++ templates/php-component/src/Component.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MyComponent;
+
+use Keboola\Component\BaseComponent;
+
+class Component extends BaseComponent
+{
+    public function run(): void
+    {
+        // @TODO implement
+    }
+}
---
+++ templates/php-component/src/MyConfig.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MyComponent;
+
+use Keboola\Component\Config\BaseConfig;
+
+class MyConfig extends BaseConfig
+{
+    // implement your custom getters
+}
