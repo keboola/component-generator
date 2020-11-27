8e4c24509a2a738d38af4882435ddd07ba4b469b
Michal Jurečko <michal.jurecko@gmail.com>
2020-11-27 12:52:23 +0100

    User common exceptions in php template

--- templates/php-component/src/run.php
+++ templates/php-component/src/run.php
@@ -2,8 +2,8 @@
 
 declare(strict_types=1);
 
-use Keboola\Component\UserException;
 use Keboola\Component\Logger;
+use Keboola\CommonExceptions\UserExceptionInterface;
 use MyComponent\Component;
 
 require __DIR__ . '/../vendor/autoload.php';
@@ -13,10 +13,10 @@ try {
     $app = new Component($logger);
     $app->execute();
     exit(0);
-} catch (UserException $e) {
+} catch (UserExceptionInterface $e) {
     $logger->error($e->getMessage());
     exit(1);
-} catch (\Throwable $e) {
+} catch (Throwable $e) {
     $logger->critical(
         get_class($e) . ':' . $e->getMessage(),
         [
