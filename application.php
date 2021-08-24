f245e07f0b9709b179d1d8b92fde45bcbba88d87
Ondra Jodas <ondrej.jodas@keboola.com>
2021-08-24 11:04:23 +0200

    review fixes

--- application.php
+++ application.php
@@ -7,13 +7,13 @@ require __DIR__ . '/vendor/autoload.php';
 use Keboola\AppSkeleton\GenerateCommand;
 use Symfony\Component\Console\Application;
 
-//try {
+try {
     $application = new Application();
     $command = new GenerateCommand();
     $application->add($command);
     $application->setDefaultCommand($command->getName(), true);
     $application->run();
-/*} catch (Throwable $e) {
+} catch (Throwable $e) {
     echo 'An error occurred ' . $e->getMessage();
 }
-*/
+
